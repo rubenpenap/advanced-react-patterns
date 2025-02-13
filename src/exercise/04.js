660c2fd422fc5ba3312beb8919225db7a6164000
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-13 13:17:03 -0300

    feat: enhance useToggle to include getTogglerProps for improved event handling and accessibility

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,19 +4,46 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn?.(args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  const togglerProps = {
+    'aria-pressed': on,
+    onClick: toggle,
+  }
+  const getTogglerProps = ({onClick, ...props} = {}) => ({
+    ...togglerProps,
+    onClick: callAll(onClick, toggle),
+    ...props,
+  })
+  return {
+    on,
+    toggle,
+    getTogglerProps,
+  }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
